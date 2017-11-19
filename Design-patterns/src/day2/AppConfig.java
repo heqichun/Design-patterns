57d170bbabddafd21d866127996b15f947a5f0cb
heqichun <747204379@qq.com>
2017-11-19 22:46:32 +0800

    2017/11/19 log

---
+++ Design-patterns/src/day2/AppConfig.java
@@ -0,0 +1,41 @@
+package day2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AppConfig {
+	private String parameterA;
+	private String praameterB;
+	public String getParameterA() {
+		return parameterA;
+	}
+	public String getPraameterB() {
+		return praameterB;
+	}
+	
+	public AppConfig(){
+		readConfig();
+	}
+	
+	private void readConfig(){
+		Properties p = new Properties();
+		InputStream in = null;
+		try {
+			in = AppConfig.class.getResourceAsStream("AppConfig.properties");
+			p.load(in);
+			this.parameterA = p.getProperty("paramA");
+			this.praameterB = p.getProperty("paramB");
+		} catch (IOException e) {
+			System.out.println("装配配置文件出错");
+			e.printStackTrace();
+		}finally {
+			try {
+				in.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+}
